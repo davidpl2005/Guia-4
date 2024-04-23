6b98d77a568fcdd5b402be55bec0c90749b0995a
Isac pacheco <isac.pachecolopez@unicolombo.edu.co>
2024-04-23 18:57:05 -0500

    Combo

---
+++ guia_4/src/guia/gui/Principal.java
@@ -0,0 +1,14 @@
+
+package guia.gui;
+
+/**
+ *
+ * @author isac pacheco
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
